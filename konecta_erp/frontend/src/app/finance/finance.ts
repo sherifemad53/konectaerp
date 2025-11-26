49417c6ae24e3306049d431e8e54feb2a9e024ac
sherifemad53 <sherifemad53@gmail.com>
2025-11-26 15:54:27 +0200

    feat: Update dashboard, inventory, finance, reporting modules and app routes, and remove Angular build cache files.

--- konecta_erp/frontend/src/app/finance/finance.ts
+++ konecta_erp/frontend/src/app/finance/finance.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-finance',
+  standalone: true,
   imports: [],
   templateUrl: './finance.html',
   styleUrl: './finance.scss'
--- konecta_erp/frontend/src/app/reporting/reporting.ts
+++ konecta_erp/frontend/src/app/reporting/reporting.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-reporting',
+  standalone: true,
   imports: [],
   templateUrl: './reporting.html',
   styleUrl: './reporting.scss'
